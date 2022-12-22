e47f82e7ff5ecfd400fefbcb75b02574b79f8690
bricheek <bricheek@gmail.com>
2022-12-22 09:58:42 -0500

    Added dweet model

--- social/dwitter/admin.py
+++ social/dwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Profile
+from .models import Dweet, Profile
 
 class ProfileInline(admin.StackedInline):
     model = Profile
@@ -14,6 +14,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
 # admin.site.register(Profile)
+admin.site.register(Dweet)
 
 
 # Register your models here.
