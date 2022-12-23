bec38ba87fe30037c91fa5ad29f8a298db30c26c
bricheek <bricheek@gmail.com>
2022-12-22 19:28:09 -0500

    post functions, fixed model, templates

--- social/dwitter/models.py
+++ social/dwitter/models.py
@@ -1,8 +1,21 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+class Dweet(models.Model):
+    user=models.ForeignKey(
+        User, related_name='dweets', on_delete=models.DO_NOTHING
+        )
+    body=models.CharField(max_length=140)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f'{self.user} '
+            f'({self.created_at:%Y-%m-%d %H:%M}): '
+            f'{self.body[:30]}...'
+        )
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,17 +39,6 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 # signal executes create profile - refactored to use receiver decorator.
 #post_save.connect(create_profile, sender=User)
-class Dweet(models.Model):
-    user=models.ForeignKey(
-        User, related_name='Dweets', on_delete=models.DO_NOTHING
-        ),
-    body=models.CharField(max_length=140)
-    created_at=models.DateTimeField(auto_now_add=True)
 
-    def __str__(self) -> str:
-        return (
-            f"{self.user} " 
-            f"({self.created_at:%Y-%m-%d %H:%M}): "
-            f"{self.body[:30]}"
-        )
+
 
